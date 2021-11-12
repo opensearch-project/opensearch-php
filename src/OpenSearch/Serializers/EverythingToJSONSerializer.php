5f1245cd1e4c4632b815f87a6418ae9a8cd8ec5b
Vijayan Balasubramanian <balasvij@amazon.com>
2021-11-11 18:58:44 -0800

    Update license headers
    Signed-off-by: Vijayan Balasubramanian <balasvij@amazon.com>

--- src/OpenSearch/Serializers/EverythingToJSONSerializer.php
+++ src/OpenSearch/Serializers/EverythingToJSONSerializer.php
@@ -3,16 +3,21 @@
 declare(strict_types=1);
 
 /**
+ * Copyright OpenSearch Contributors
  * SPDX-License-Identifier: Apache-2.0
  *
- * The OpenSearch Contributors require contributions made to
- * this file be licensed under the Apache-2.0 license or a
- * compatible open source license.
+ * Elasticsearch PHP client
  *
- * Modifications Copyright OpenSearch Contributors. See
- * GitHub history for details.
+ * @link      https://github.com/elastic/elasticsearch-php/
+ * @copyright Copyright (c) Elasticsearch B.V (https://www.elastic.co)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license   https://www.gnu.org/licenses/lgpl-2.1.html GNU Lesser General Public License, Version 2.1
+ *
+ * Licensed to Elasticsearch B.V under one or more agreements.
+ * Elasticsearch B.V licenses this file to you under the Apache 2.0 License or
+ * the GNU Lesser General Public License, Version 2.1, at your option.
+ * See the LICENSE file in the project root for more information.
  */
-
 namespace OpenSearch\Serializers;
 
 use OpenSearch\Common\Exceptions\RuntimeException;
