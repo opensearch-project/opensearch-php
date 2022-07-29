3c9c2f452846f260d5dcc214e6bd5360077bf366
Mustafa Akçakaya <mgsmus@gmail.com>
2022-07-29 22:11:56 +0300

    Allow psr/log 3 (#72)
    * Allow psr/log 3
    
    Signed-off-by: Mustafa Akçakaya <mgsmus@gmail.com>
    
    * fix: logger for Logger v3
    
    Signed-off-by: Soner Sayakci <s.sayakci@shopware.com>
    
    Co-authored-by: Soner Sayakci <s.sayakci@shopware.com>

--- tests/ClientBuilder/ArrayLogger.php
+++ tests/ClientBuilder/ArrayLogger.php
@@ -32,47 +32,47 @@ class ArrayLogger implements LoggerInterface
      */
     public $output = [];
 
-    public function emergency($message, array $context = array())
+    public function emergency($message, array $context = array()): void
     {
         $this->log(LogLevel::EMERGENCY, $message, $context);
     }
 
-    public function alert($message, array $context = array())
+    public function alert($message, array $context = array()): void
     {
         $this->log(LogLevel::ALERT, $message, $context);
     }
 
-    public function critical($message, array $context = array())
+    public function critical($message, array $context = array()): void
     {
         $this->log(LogLevel::CRITICAL, $message, $context);
     }
 
-    public function error($message, array $context = array())
+    public function error($message, array $context = array()): void
     {
         $this->log(LogLevel::ERROR, $message, $context);
     }
 
-    public function warning($message, array $context = array())
+    public function warning($message, array $context = array()): void
     {
         $this->log(LogLevel::WARNING, $message, $context);
     }
 
-    public function notice($message, array $context = array())
+    public function notice($message, array $context = array()): void
     {
         $this->log(LogLevel::NOTICE, $message, $context);
     }
 
-    public function info($message, array $context = array())
+    public function info($message, array $context = array()): void
     {
         $this->log(LogLevel::INFO, $message, $context);
     }
 
-    public function debug($message, array $context = array())
+    public function debug($message, array $context = array()): void
     {
         $this->log(LogLevel::DEBUG, $message, $context);
     }
 
-    public function log($level, $message, array $context = array())
+    public function log($level, $message, array $context = array()): void
     {
         $this->output[] = sprintf("%s: %s %s", $level, $message, json_encode($context));
     }
