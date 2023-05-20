8b6cdbce1c5c2436fa2458fc1c9f698d0fc760c7
Lukas Jansen <33984099+lukas-jansen@users.noreply.github.com>
2023-05-20 18:57:42 +0200

    Add refresh search analyzers api endpoint (#154)
    * Add refresh search analyzers api endpoint
    
    Signed-off-by: Lukas Jansen <33984099+lukas-jansen@users.noreply.github.com>
    Signed-off-by: GitHub <noreply@github.com>
    
    * Add unit test for RefreshSearchAnalyzers endpoint
    
    Signed-off-by: Lukas Jansen <jansen.lukas1998@gmail.com>
    Signed-off-by: GitHub <noreply@github.com>
    
    * Add changelog + fix phpstan
    
    Signed-off-by: Lukas Jansen <33984099+lukas-jansen@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Lukas Jansen <33984099+lukas-jansen@users.noreply.github.com>
    Signed-off-by: GitHub <noreply@github.com>
    Signed-off-by: Lukas Jansen <jansen.lukas1998@gmail.com>

---
+++ src/OpenSearch/Endpoints/Indices/RefreshSearchAnalyzers.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Elasticsearch PHP client
+ *
+ * @link      https://github.com/elastic/elasticsearch-php/
+ * @copyright Copyright (c) Elasticsearch B.V (https://www.elastic.co)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license   https://www.gnu.org/licenses/lgpl-2.1.html GNU Lesser General Public License, Version 2.1
+ *
+ * Licensed to Elasticsearch B.V under one or more agreements.
+ * Elasticsearch B.V licenses this file to you under the Apache 2.0 License or
+ * the GNU Lesser General Public License, Version 2.1, at your option.
+ * See the LICENSE file in the project root for more information.
+ */
+
+namespace OpenSearch\Endpoints\Indices;
+
+use OpenSearch\Common\Exceptions\RuntimeException;
+use OpenSearch\Endpoints\AbstractEndpoint;
+
+class RefreshSearchAnalyzers extends AbstractEndpoint
+{
+    public function getURI(): string
+    {
+        $index = $this->index ?? null;
+
+        if (isset($index)) {
+            return "/_plugins/_refresh_search_analyzers/$index";
+        }
+        throw new RuntimeException('Missing index parameter for the endpoint indices.refresh_search_analyzers');
+    }
+
+    public function getParamWhitelist(): array
+    {
+        return [];
+    }
+
+    public function getMethod(): string
+    {
+        return 'POST';
+    }
+}
---
+++ tests/Endpoints/RefreshSearchAnalyzersTest.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Elasticsearch PHP client
+ *
+ * @link      https://github.com/elastic/elasticsearch-php/
+ * @copyright Copyright (c) Elasticsearch B.V (https://www.elastic.co)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license   https://www.gnu.org/licenses/lgpl-2.1.html GNU Lesser General Public License, Version 2.1
+ *
+ * Licensed to Elasticsearch B.V under one or more agreements.
+ * Elasticsearch B.V licenses this file to you under the Apache 2.0 License or
+ * the GNU Lesser General Public License, Version 2.1, at your option.
+ * See the LICENSE file in the project root for more information.
+ */
+
+namespace OpenSearch\Tests\Endpoints;
+
+use OpenSearch\Common\Exceptions\RuntimeException;
+use OpenSearch\Endpoints\Indices\RefreshSearchAnalyzers;
+
+class RefreshSearchAnalyzersTest extends \PHPUnit\Framework\TestCase
+{
+    /** @var RefreshSearchAnalyzers */
+    private $instance;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        // Instance
+        $this->instance = new RefreshSearchAnalyzers();
+    }
+
+    public function testGetURIWhenIndexAndIdAreDefined(): void
+    {
+        // Arrange
+        $expected = '/_plugins/_refresh_search_analyzers/index';
+
+        $this->instance->setIndex('index');
+        $this->instance->setId(10);
+
+        // Act
+        $result = $this->instance->getURI();
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetURIWhenIndexIsDefinedAndIdIsNotDefined(): void
+    {
+        // Arrange
+        $expected = '/_plugins/_refresh_search_analyzers/index';
+
+        $this->instance->setIndex('index');
+
+        // Act
+        $result = $this->instance->getURI();
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetURIWhenIndexIsNotDefined(): void
+    {
+        // Arrange
+        $expected = RuntimeException::class;
+        $expectedMessage = 'Missing index parameter for the endpoint indices.refresh_search_analyzers';
+
+        // Assert
+        $this->expectException($expected);
+        $this->expectExceptionMessage($expectedMessage);
+
+        // Act
+        $this->instance->getURI();
+    }
+
+    public function testGetMethodWhenIdIsDefined(): void
+    {
+        // Arrange
+        $expected = 'POST';
+
+        $this->instance->setId(10);
+
+        // Act
+        $result = $this->instance->getMethod();
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testGetMethodWhenIdIsNotDefined(): void
+    {
+        // Arrange
+        $expected = 'POST';
+
+        // Act
+        $result = $this->instance->getMethod();
+
+        // Assert
+        $this->assertEquals($expected, $result);
+    }
+}
