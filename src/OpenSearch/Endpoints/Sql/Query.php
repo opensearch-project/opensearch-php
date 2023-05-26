98d23fd37e37b35b84e62c1813aeb4576f9441f2
archipelweb <github@archipel-web.com>
2023-05-26 15:47:53 +0200

    format on Sql

--- src/OpenSearch/Endpoints/Sql/Query.php
+++ src/OpenSearch/Endpoints/Sql/Query.php
@@ -19,7 +19,7 @@ class Query extends AbstractEndpoint
 {
     public function getParamWhitelist(): array
     {
-        return [];
+        return ['format'];
     }
 
     public function getURI(): string
