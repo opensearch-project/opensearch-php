686911fccbffc1e911dd26a269e52613a2e4e982
saimedhi <saimedhi@amazon.com>
2024-06-20 09:33:50 -0700

    Moved Endpoint Proxies in Namespace Generator to Separate Folder
    Signed-off-by: saimedhi <saimedhi@amazon.com>

---
+++ util/EndpointProxies/tasks/tasksListProxy.php
@@ -0,0 +1,12 @@
+<?php
+
+return <<<'EOD'
+
+/**
+ * Proxy function to list() to prevent BC break since 7.4.0
+ */
+public function tasksList(array $params = [])
+{
+    return $this->list($params);
+}
+EOD;
