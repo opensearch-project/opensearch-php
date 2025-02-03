455b7f1e682a442220a33724484f877c4927ca47
Kim Pepper <kim@pepper.id.au>
2025-02-03 18:33:45 +1100

    Add php cs fixer rule to remove unused imports (#275)
    Signed-off-by: Kim Pepper <kim@pepper.id.au>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 This document has been generated with
 https://mlocati.github.io/php-cs-fixer-configurator/#version:3.0.0-rc.1|configurator
@@ -9,10 +10,12 @@ return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
+        'no_unused_imports' => true,
     ])
-    ->setFinder(PhpCsFixer\Finder::create()
+    ->setFinder(
+        PhpCsFixer\Finder::create()
         ->exclude('vendor')
         ->exclude('util/cache')
         ->in(__DIR__)
     )
-    ;
+;
