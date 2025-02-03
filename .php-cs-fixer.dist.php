b7d6da1a1a4af189fad36366be9e62c1d23cc7f7
Kim Pepper <kim@pepper.id.au>
2025-02-04 09:40:54 +1100

    Fix imports and whitespace
    Signed-off-by: Kim Pepper <kim@pepper.id.au>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,19 +1,28 @@
 <?php
 
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+
 /*
 This document has been generated with
 https://mlocati.github.io/php-cs-fixer-configurator/#version:3.0.0-rc.1|configurator
 you can change this configuration by importing this file.
  */
 
-return (new PhpCsFixer\Config())
+return (new Config())
     ->setRiskyAllowed(true)
     ->setRules([
         '@PSR12' => true,
         'no_unused_imports' => true,
+        'fully_qualified_strict_types' => [
+            'import_symbols' => true,
+            'leading_backslash_in_global_namespace' => true,
+            'phpdoc_tags' => ['property-read', 'property-write'],
+        ],
+        'no_extra_blank_lines' => ['tokens' => ['extra']],
     ])
     ->setFinder(
-        PhpCsFixer\Finder::create()
+        Finder::create()
         ->exclude('vendor')
         ->exclude('util/cache')
         ->in(__DIR__)
